d48b508a1eecd14a5eb5ae6d46c69dd6d78e522b
Diogo Ribeiro <dribeiro641@gmail.com>
2022-01-23 23:36:22 +0000

    articles accordion done

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,8 +1,60 @@
-$(".article").click(function(event) {
-  let text = $(this).find('.article-text')
+var acc = document.getElementsByClassName("article");
 
-  $(event.target.tagName).find('.article-text').not(this).fadeOut();
-  
-  text.fadeIn();
-  text.css('transform' , 'none');
-});
+function myFunction(x) {
+  if (x.matches) {
+    for (var i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        var panel = this.nextElementSibling;
+        var allPanels = document.querySelectorAll('.panel');
+
+        if(panel == allPanels[0]) {
+          allPanels[1].style.maxWidth = null;
+          allPanels[1].previousElementSibling.classList.remove("active");
+        }
+        else if(panel == allPanels[1]) {
+          allPanels[0].style.maxWidth = null;
+          allPanels[0].previousElementSibling.classList.remove("active");
+        }
+        
+        if (panel.style.maxWidth) {
+          this.classList.remove("active");
+          panel.style.maxWidth = null;
+        } else {
+          this.classList.add("active");
+          panel.style.maxWidth = "100%";
+          panel.querySelector('.article-text').style.opacity = 1;
+        }
+      });
+    }
+  }
+  else{
+    for (var i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        var panel = this.nextElementSibling;
+        var allPanels = document.querySelectorAll('.panel');
+
+        if(panel == allPanels[0]) {
+          allPanels[1].style.maxHeight = null;
+          allPanels[1].previousElementSibling.classList.remove("active");
+        }
+        else if(panel == allPanels[1]) {
+          allPanels[0].style.maxHeight = null;
+          allPanels[0].previousElementSibling.classList.remove("active");
+        }
+        
+        if (panel.style.maxHeight) {
+          this.classList.remove("active");
+          panel.style.maxHeight = null;
+        } else {
+          this.classList.add("active");
+          panel.style.maxHeight = "100%";
+          panel.querySelector('.article-text').style.opacity = 1;
+        }
+      });
+    }
+  }
+}
+
+var x = window.matchMedia("(min-width: 1024px)")
+myFunction(x)
+x.addListener(myFunction)
